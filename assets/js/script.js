7179c3fbbf13638b9b8ba9a1e61097185487cb69
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-04-16 16:56:23 -0500

    finalized UI and updated readMe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,13 @@
 // GLOBAL VARIABLES
+// THIS CORRESPONDS TO WHERE CURRENT TIME TICKER WILL DISPLAY IN HTML/ ON PAGE
 
-var timeDisplay = $('#currentDay'); 
+var timeDisplay = $('#currentTick'); 
 
-
-// DISPLAY CURRENT DATE & TIME IN HEADER
-
-let timeShow = $("#currentDay");
+// DISPLAY CURRENT DATE & TIME IN HEADER (FORMAT MUST BE EX. SEP 16, 2023 at 2:15:22am -- STARTED CODE SUGGESTED hh:mm:ss BUT I THINK 02:15:22am LOOKS WEIRD)
 
 function showTime() {
-  let rn = dayjs().format(
-    "MMM DD, YYYY [at] hh:mm:ss a"
-  );
-  timeShow.text(rn);
+  var rightNow = dayjs().format("MMM DD, YYYY [at] h:mm:ss a");
+  timeDisplay.text(rightNow);
 }
 
 showTime();
@@ -19,10 +15,9 @@ setInterval(showTime, 1000);
 
 // SET SAVE BUTTONS AND STORE USER INPUT TO LOCAL STORAGE
 
-$(".saveBtn").click(function () {
-  let description = $(this).siblings(".description").val();
-  
-  let inputTime = $(this).parent().attr("id").split("-")[1];
+$(".saveBtn").on('click', function () {
+  var description = $(this).siblings(".description").val();
+  var inputTime = $(this).parent().attr("id").split("-")[1];
   
   localStorage.setItem(inputTime, description);
   
@@ -31,20 +26,22 @@ $(".saveBtn").click(function () {
 // ASSIGN ROW COLOR BASED ON TIME-FROM-NOW
 
 $(".time-block").each(function () {
-  let hourHour = dayjs().format("HH");
+  let timeHour = dayjs().format("HH");
   let rowHour = parseInt($(this).attr("id").split("-")[1]);
 
-  if (rowHour === hourHour) {
+  if (rowHour == timeHour) {
     $(this).addClass("present");
-  } else if (rowHour > hourHour) {
+  } else if (rowHour > timeHour) {
     $(this).addClass("future");
   } else {
     $(this).addClass("past");
   }
 });
 
-$("#hour-08 .description").val(localStorage.getItem("08"));
-$("#hour-09 .description").val(localStorage.getItem("09"));
+// PULL VALUES FROM LOCAL STORAGE
+
+$("#hour-8 .description").val(localStorage.getItem("8"));
+$("#hour-9 .description").val(localStorage.getItem("9"));
 $("#hour-10 .description").val(localStorage.getItem("10"));
 $("#hour-11 .description").val(localStorage.getItem("11"));
 $("#hour-12 .description").val(localStorage.getItem("12"));
@@ -54,6 +51,10 @@ $("#hour-15 .description").val(localStorage.getItem("15"));
 $("#hour-16 .description").val(localStorage.getItem("16"));
 $("#hour-17 .description").val(localStorage.getItem("17"));
 
+// CONSOLE LOG AND TEST IN-APP TO SEE IF INPUT IS SAVED TO LOCAL STORAGE
+
+console.log(localStorage);
+
 // OPTION TO CLEAR LOCAL STORAGE
 
 $(".clearBtn").click(function () {
@@ -61,3 +62,5 @@ $(".clearBtn").click(function () {
 
   localStorage.clear();
 });
+
+// CLEARED LOCAL STORAGE AND ABOVE CONSOLE LOG SHOWED IT AS EMPTY! :)
